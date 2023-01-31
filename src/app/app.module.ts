e8a9d93083248acbcd4e2a346fdd0b44b85514ac
param-ninthu <ninthu1999@gmail.com>
2023-01-31 22:19:39 +0530

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HomeModule
     
   ],
   providers: [],
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedRoutingModule } from './shared-routing.module';
+import { ServicesRoutingModule } from './services-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    ServicesRoutingModule
   ]
 })
-export class SharedModule { }
+export class ServicesModule { }
